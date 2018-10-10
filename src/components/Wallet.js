905044e442d3358a9d974ae842a34aa454d112a2
Cagney <CagnGe01@risk.regn.net>
2018-10-10 15:53:52 +0100

    tests for deposit and withraw

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export class Wallet extends Component {
+  constructor() {
+    super();
+    this.state = {
+      balance: undefined,
+    };
+  }
+
+  updateBalance = event => {
+    const balance = parseInt(event.target.value, 10);
+    this.setState({ balance });
+  };
+
   render() {
     return (
       <div>
         <h3 className="balance">Wallet Balance: {this.props.balance}</h3>
+        <br />
+        <input className="input-wallet" onChange={this.updateBalance} />
       </div>
     );
   }
 }
 
-// export default connect((state) => { return { balance: state } }, null)(Wallet);
-export default connect(state => { return { balance: state } }, null)(Wallet);
+export default connect(
+  state => {
+    return { balance: state };
+  },
+  null
+)(Wallet);
--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -1,12 +1,30 @@
-import balanceReducer from "./balance";
-import * as constants from "../actions/constants";
+import balanceReducer from './balance';
+import * as constants from '../actions/constants';
 
-describe("balanceReducer", () => {
-  it("sets a balance", () => {
+describe('balanceReducer', () => {
+  it('sets a balance', () => {
     const balance = 10;
 
     expect(
       balanceReducer(undefined, { type: constants.SET_BALANCE, balance })
     ).toEqual(balance);
+  });
+
+  it('deposits into the balance', () => {
+    const deposit = 10;
+    const initialState = 5;
+
+    expect(
+      balanceReducer(initialState, { type: constants.DEPOSIT, deposit })
+    ).toEqual(initialState + deposit);
+  });
+
+  it('withdraws from the balance', () => {
+    const withdraw = 5;
+    const initialState = 10;
+
+    expect(
+      balanceReducer(initialState, { type: constants.WITHDRAW, withdraw })
+    ).toEqual(initialState - withdraw);
   });
 });
